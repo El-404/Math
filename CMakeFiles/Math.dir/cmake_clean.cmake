a02d4e2480fadb0efc6512d86d897c03c8ce37e3
= <=>
2026-03-06 14:15:41 -0600

    cmake and eigen

---
+++ CMakeFiles/Math.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Math.dir/link.d"
+  "CMakeFiles/Math.dir/src/main.cpp.o"
+  "CMakeFiles/Math.dir/src/main.cpp.o.d"
+  "Math"
+  "Math.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Math.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cmath>
+#include <functional>
+#include <Core>
+
+double nthDerive(std::function<double(double)> function, double x, int n) {
+  double h = .01;
+
+  printf("%f\n", (function(x + h) - function(x-h)) / (2*h) );
+
+  if (n == 1) return (function(x + h) - function(x-h)) / (2*h);
+
+  std::function<double(double)> derivedFunction = [function, h](double x) {
+    return (function(x + h) - function(x-h)) / (2*h);
+  };
+  return nthDerive(derivedFunction, x, n-1);
+}
+
+int main() {
+  std::function<double(double)> func = [](double x) { return sin(x); };
+  nthDerive(func, 0, 12);
+}
